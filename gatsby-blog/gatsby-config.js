d8748ee9f111b4638a65f524057c157041162abf
dhrupad <dhrupad1912@gmail.com>
2021-09-25 17:36:46 +0530

    dhrupad

---
+++ gatsby-blog/gatsby-config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  siteMetadata: {
+    title: `Blog`,
+    description: `hi`,
+    author: `@gatsby's`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-sass',
+    'gatsby-plugin-catch-links',
+    'gatsby-transformer-remark',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+      },
+    },
+  ],
+}
---
+++ gatsby-blog/src/pages/about.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+
+const AboutPage = () => (
+  <Layout pageTitle="About us">
+    <SEO title="About" keywords={[`gatsby`, `application`, `react`]} />
+    <p>
+     ABOUT AS
+    </p>
+  </Layout>
+)
+
+export default AboutPage
